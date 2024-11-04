941f9701bf9d8bbc932be2deebc0ccc09aa245dc
sachin-s <sachins1221@gmail.com>
2024-11-04 12:46:26 +0530

    Fonts updated

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../fonts
 //= link_tree ../images
+//= link_tree ../stylesheets
 //= link_tree ../builds
--- tailwind.config.js
+++ tailwind.config.js
@@ -62,7 +62,7 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["var(--font-sans)",'Geist', ...fontFamily.sans],
+        sans: ["Geist","var(--font-sans)", ...fontFamily.sans],
       },
       keyframes: {
         "accordion-down": {
